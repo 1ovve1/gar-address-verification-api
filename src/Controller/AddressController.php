b1dc59a22bd0d7befe15ff5a03edd1d2e909343c
lowel <10vvvvvv31@gmail.com>
2022-05-28 17:21:27 +0300

    write simple request tests

--- src/Controller/AddressController.php
+++ src/Controller/AddressController.php
@@ -22,17 +22,31 @@ class AddressController
   public function getAddressByName(Request $request, Response $response, $args): Response
   {
     $param = $request->getQueryParams();
-    if (array_key_exists('address', $param)) {
-      $halfAddress = explode(',', $param['address']);
-      $likeAddress[] = $this->addressByNameRepo->getFullAddress($halfAddress);
 
-      if (empty($likeAddress)) {
-        $response = $this->errorMessage($response, 'address not found');
-      } else {
-        $response->getBody()->write(json_encode($likeAddress, JSON_FORCE_OBJECT));
-      }
+    if (!array_key_exists('address', $param)) {
+      $response = $this->errorMessage($response, "require 'address' param", 406);
+      return $response;
     } else {
-      $response = $this->errorMessage($response, "require 'address' param");
+      if (strlen($param['address']) >= 1000) {
+        $response = $this->errorMessage($response, "param 'address' too large, canceld", 414);
+        return $response;
+      }
+    }
+
+    $halfAddress = explode(',', $param['address']);
+
+    if (count($halfAddress) > 1 && empty(trim($halfAddress[0]))) {
+      $response = $this->errorMessage($response, "param 'address' shouldn't be empty", 411);
+      return $response;
+    }
+       
+    $likeAddress[] = $this->addressByNameRepo->getFullAddress($halfAddress);
+
+    if (!empty($likeAddress[0])) {
+      $response->getBody()->write(json_encode($likeAddress, JSON_FORCE_OBJECT));
+    } else {
+      $response = $this->errorMessage($response, 'address not found', 404);
+      return $response;
     }
 
     return $response;
@@ -50,29 +64,38 @@ class AddressController
         explode(',', $param['address'])
       );
 
-      while($lastElem = array_pop($fullAddress)) {
-        if (count($lastElem) === 1) {
-          $objectId = $lastElem[0]['objectid'];
-          break;
+      if (!key_exists('error', $fullAddress)) {
+        while($lastElem = array_pop($fullAddress)) {
+          if (count($lastElem) === 1) {
+            $objectId = $lastElem[0]['objectid'];
+            break;
+          }
         }
-      }
+      } 
     } else {
-      $response = $this->errorMessage($response, "require 'objectid' or 'address' param");
+      $response = $this->errorMessage($response, "require 'objectid' or 'address' param", 406);
+      return $response;
     }
 
     if (!is_null($objectId)) {
       $data = $this->addressByCodeRepo->getCode(
         $objectId, $args['type']
       );
-      $response->getBody()->write(json_encode($data, JSON_PRETTY_PRINT));
+
+      if (empty($data)) {
+        $response = $this->errorMessage($response, "codes not found", 404); 
+      } else {
+        $response->getBody()->write(json_encode($data, JSON_PRETTY_PRINT));
+      }
     } else {
-      $response = $this->errorMessage($response, "address not found");
+      $response = $this->errorMessage($response, 'address not found', 404);
     }
 
-    return $response;
+    return $response; 
   }
 
-  protected function errorMessage(Response $response, string $message, int $status = 401) : Response
+
+  protected function errorMessage(Response $response, string $message, int $status = 400) : Response
   {
     $response = $response->withStatus($status);
     $response->getBody()->write(json_encode(['error' => $message]));
---
+++ tests/Integration/AddressCodeTest.php
@@ -0,0 +1,205 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Integration;
+
+
+class AddressCodeTest extends BaseTestSetup
+{
+	public function testAllCodesByName() 
+	{
+		$response = $this->runApp('GET', '/code/all', 'address=калм,лаган,кр,кра,школьная');
+		$data = (string) $response->getBody();
+
+		$data = preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
+    		return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UTF-16BE');
+		}, $data);
+
+
+		$this->assertEquals(200, $response->getStatusCode());
+		$this->assertEquals('application/json', $response->getHeaderLine('Content-Type'));
+		$this->assertStringContainsString('OKATO', $data);
+		$this->assertStringContainsString('OKTMO', $data);
+		$this->assertStringContainsString('KLADR', $data);
+		$this->assertStringNotContainsString('error', $data);
+	}	
+
+	public function testAllCodesByObjectId() 
+	{
+		$response = $this->runApp('GET', '/code/all', 'objectid=109874');
+		$data = (string) $response->getBody();
+
+		$data = preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
+    		return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UTF-16BE');
+		}, $data);
+
+
+		$this->assertEquals(200, $response->getStatusCode());
+		$this->assertEquals('application/json', $response->getHeaderLine('Content-Type'));
+		$this->assertStringContainsString('OKATO', $data);
+		$this->assertStringContainsString('OKTMO', $data);
+		$this->assertStringContainsString('KLADR', $data);
+		$this->assertStringNotContainsString('error', $data);
+	}
+
+	public function testOkatoByName() 
+	{
+		$response = $this->runApp('GET', '/code/okato', 'address=калм,лаган,кр,кра,школьная');
+		$data = (string) $response->getBody();
+
+		$data = preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
+    		return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UTF-16BE');
+		}, $data);
+
+
+		$this->assertEquals(200, $response->getStatusCode());
+		$this->assertEquals('application/json', $response->getHeaderLine('Content-Type'));
+		$this->assertStringContainsString('OKATO', $data);
+		$this->assertStringNotContainsString('OKTMO', $data);
+		$this->assertStringNotContainsString('KLADR', $data);
+		$this->assertStringNotContainsString('error', $data);
+	}	
+
+	public function testOkatoByObjectId() 
+	{
+		$response = $this->runApp('GET', '/code/okato', 'objectid=109874');
+		$data = (string) $response->getBody();
+
+		$data = preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
+    		return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UTF-16BE');
+		}, $data);
+
+
+		$this->assertEquals(200, $response->getStatusCode());
+		$this->assertEquals('application/json', $response->getHeaderLine('Content-Type'));
+		$this->assertStringContainsString('OKATO', $data);
+		$this->assertStringNotContainsString('OKTMO', $data);
+		$this->assertStringNotContainsString('KLADR', $data);
+		$this->assertStringNotContainsString('error', $data);
+	}
+
+	public function testOktmoByName() 
+	{
+		$response = $this->runApp('GET', '/code/oktmo', 'address=калм,лаган,кр,кра,школьная');
+		$data = (string) $response->getBody();
+
+		$data = preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
+    		return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UTF-16BE');
+		}, $data);
+
+
+		$this->assertEquals(200, $response->getStatusCode());
+		$this->assertEquals('application/json', $response->getHeaderLine('Content-Type'));
+		$this->assertStringNotContainsString('OKATO', $data);
+		$this->assertStringContainsString('OKTMO', $data);
+		$this->assertStringNotContainsString('KLADR', $data);
+		$this->assertStringNotContainsString('error', $data);
+	}	
+
+	public function testOktmoByObjectId() 
+	{
+		$response = $this->runApp('GET', '/code/oktmo', 'objectid=109874');
+		$data = (string) $response->getBody();
+
+		$data = preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
+    		return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UTF-16BE');
+		}, $data);
+
+
+		$this->assertEquals(200, $response->getStatusCode());
+		$this->assertEquals('application/json', $response->getHeaderLine('Content-Type'));
+		$this->assertStringNotContainsString('OKATO', $data);
+		$this->assertStringContainsString('OKTMO', $data);
+		$this->assertStringNotContainsString('KLADR', $data);
+		$this->assertStringNotContainsString('error', $data);
+	}
+
+	public function testKladrByName() 
+	{
+		$response = $this->runApp('GET', '/code/kladr', 'address=калм,лаган,кр,кра,школьная');
+		$data = (string) $response->getBody();
+
+		$data = preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
+    		return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UTF-16BE');
+		}, $data);
+
+
+		$this->assertEquals(200, $response->getStatusCode());
+		$this->assertEquals('application/json', $response->getHeaderLine('Content-Type'));
+		$this->assertStringNotContainsString('OKATO', $data);
+		$this->assertStringNotContainsString('OKTMO', $data);
+		$this->assertStringContainsString('KLADR', $data);
+		$this->assertStringNotContainsString('error', $data);
+	}	
+
+	public function testKladrByObjectId() 
+	{
+		$response = $this->runApp('GET', '/code/kladr', 'objectid=109874');
+		$data = (string) $response->getBody();
+
+		$data = preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
+    		return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UTF-16BE');
+		}, $data);
+
+
+		$this->assertEquals(200, $response->getStatusCode());
+		$this->assertEquals('application/json', $response->getHeaderLine('Content-Type'));
+		$this->assertStringNotContainsString('OKATO', $data);
+		$this->assertStringNotContainsString('OKTMO', $data);
+		$this->assertStringContainsString('KLADR', $data);
+		$this->assertStringNotContainsString('error', $data);
+	}
+
+	public function testWrongAddressName() 
+	{
+		$response = $this->runApp('GET', '/code/kladr', 'address=French,Paris');
+		$data = (string) $response->getBody();
+
+		$data = preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
+    		return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UTF-16BE');
+		}, $data);
+
+
+		$this->assertEquals(404, $response->getStatusCode());
+		$this->assertEquals('application/json', $response->getHeaderLine('Content-Type'));
+		$this->assertStringNotContainsString('OKATO', $data);
+		$this->assertStringNotContainsString('OKTMO', $data);
+		$this->assertStringNotContainsString('KLADR', $data);
+		$this->assertStringContainsString('error', $data);
+	}
+
+	public function testWrongObjectId() 
+	{
+		$response = $this->runApp('GET', '/code/kladr', 'objectid=0');
+		$data = (string) $response->getBody();
+
+		$data = preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
+    		return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UTF-16BE');
+		}, $data);
+
+
+		$this->assertEquals(404, $response->getStatusCode());
+		$this->assertEquals('application/json', $response->getHeaderLine('Content-Type'));
+		$this->assertStringNotContainsString('OKATO', $data);
+		$this->assertStringNotContainsString('OKTMO', $data);
+		$this->assertStringNotContainsString('KLADR', $data);
+		$this->assertStringContainsString('error', $data);
+	}
+
+	public function testIncorectParamsName() 
+	{
+		$response = $this->runApp('GET', '/code/kladr', 'dfdf=0&sdsd=2');
+		$data = (string) $response->getBody();
+
+		$data = preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
+    		return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UTF-16BE');
+		}, $data);
+
+
+		$this->assertEquals(406, $response->getStatusCode());
+		$this->assertEquals('application/json', $response->getHeaderLine('Content-Type'));
+		$this->assertStringNotContainsString('OKATO', $data);
+		$this->assertStringNotContainsString('OKTMO', $data);
+		$this->assertStringNotContainsString('KLADR', $data);
+		$this->assertStringContainsString('error', $data);	
+	}
+}
