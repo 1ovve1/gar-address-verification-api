a0d8b4d9ca0ea53d99980310fa68685d5c05a87b
Andrey Bulavskiy <10vvvvvv31@gmail.com>
2022-07-27 19:58:03 +0300

    fix some issues

--- src/Middleware/CodeMiddleware.php
+++ src/Middleware/CodeMiddleware.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace GAR\Middleware;
 
 use GAR\Repository\Codes;
-use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Psr7\Response;
 use Slim\Routing\RouteContext;
 
 class CodeMiddleware
 {
-    public function __invoke(Request $request, RequestHandler $handler): Response
+    public function __invoke(Request $request, RequestHandler $handler): \Psr\Http\Message\ResponseInterface
     {
         $args = RouteContext::fromRequest($request)->getRoute()->getArguments();
 
