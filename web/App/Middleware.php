2da6c1410e3f8b618943e169bdc15d390b4b8bf8
Andrey Bulavskiy <10vvvvvv31@gmail.com>
2022-07-31 13:26:28 +0300

    add use state

--- web/App/Middleware.php
+++ web/App/Middleware.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
   
 use GAR\Logger\Log;
+use GAR\Middleware\BeforeMiddleware;
 
 return function ($app) {
     $app->addRoutingMiddleware();
 
 
-    $app->add(\GAR\Middleware\BeforeMiddleware::class);
+    $app->add(BeforeMiddleware::class);
 
     $app->addErrorMiddleware(
         filter_var($_ENV['DISPLAY_ERROR_DETAILS'], FILTER_VALIDATE_BOOLEAN),
