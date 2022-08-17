3b585d37f49b40433d95fc395d9713ce38c2b19c
Andrey Bulavskiy <10vvvvvv31@gmail.com>
2022-08-17 15:44:28 +0300

    create migrate able file and QueryGenerator (gen create table query) test

---
+++ database/ORM/Migration/MigrateAble.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace DB\ORM\Migration;
+
+interface MigrateAble
+{
+
+}
---
+++ tests/Unit/QueryGeneratorTest.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use DB\ORM\Migration\Container\QueryGenerator;
+use PHPUnit\Framework\TestCase;
+
+class QueryGeneratorTest extends TestCase
+{
+
+	public function testMakeCreateTableQuery()
+	{
+
+	}
+}
