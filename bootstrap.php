e33bf85023b388422e814c2bfda1232c00a47d9c
Andrey Bulavskiy <10vvvvvv31@gmail.com>
2022-09-25 18:41:42 +0300

    bug fix and refactor for bootstrap file (before it isnt load test config)

--- bootstrap.php
+++ bootstrap.php
@@ -4,40 +4,51 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
-
-if (defined('TEST_ENV')) {
-	$envName = '.env.test';
-	$basePath = str_replace('.env.test', '', TEST_ENV);
-} else {
+// check phpunit context
+if (!defined('PHPUNIT_TEST_RUNTIME')) {
+	// load env
 	$envName = '.env';
 	$basePath = __DIR__ . '/';
+
+	// add error handler
+	require_once __DIR__ . '/default_handler_and_logger.php';
+} else {
+	// load env.test
+	$envName = '.env.test';
+	$basePath = __DIR__ . '/tests/';
 }
 
-// prepare and read data from /.env file
-$dotenv = Dotenv::createImmutable($basePath, $envName);
-if (!file_exists($basePath . $envName)) {
-	echo "Error while read '{$envName}' file in '{$basePath}'" . PHP_EOL;
-	exit(-1);
+// load env
+loadEnv($basePath, $envName);
+
+// check cli context
+if (!defined('SERVER_START')){
+	require_once __DIR__ . '/cli/cli_bootstrap.php';
 }
-$dotenv->load();
 
-// update some env values with __DIR__ prefix
 
-foreach ($_ENV as $index => &$param) {
-	if (is_string($param)) {
+// some of func here
+
+/**
+ * env load procedure
+ * @param string $basePath - path to the env file
+ * @param string $envName - name of env file ('.env' by default)
+ * @return void
+ */
+function loadEnv(string $basePath, string $envName = '.env'): void
+{
+	// prepare and read data from /.env file
+	$dotenv = Dotenv::createImmutable($basePath, $envName);
+	try{
+		$dotenv->load();
+	} catch (\Dotenv\Exception\InvalidPathException $e) {
+		throw new RuntimeException("Name: '{$envName}'\nPath: '{$basePath}'\nMessage: '{$e->getMessage()}'", $e->getCode(), $e);
+	}
+
+	// update some env values with __DIR__ prefix
+	foreach ($_ENV as $index => &$param) {
 		if (preg_match('/^.*_PATH$/', $index)) {
 			$param = __DIR__ . $param;
 		}
 	}
-}
-
-// add error handler
-
-if (!defined('PHPUNIT_TEST_RUNTIME')) {
-	require_once __DIR__ . '/default_handler_and_logger.php';
-}
-
-// check context (cli or web)
-if (!defined('SERVER_START')){
-	require_once __DIR__ . '/cli/cli_bootstrap.php';
 }
